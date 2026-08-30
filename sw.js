31d41c22102ce5592d396c4bdb0aeec30813e294
45kikurage-rgb <45kikurage@gmail.com>
2026-08-30 10:50:39 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-const CACHE='asset-manager-v2-icons';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='asset-manager-v2-icons-20260830e';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-any-20260830e.png','./icon-maskable-20260830e.png','./slime-icon-20260830e.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys()
+    .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    .then(()=>self.clients.claim())
+));
 self.addEventListener('fetch',e=>{
  if(e.request.method!=='GET')return;
  e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
